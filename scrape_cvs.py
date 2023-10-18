288614c6f4b57ef0786a5d38302575f6cf84943f
Saksham Solanki <saksham.lussqvx.github@gmail.com>
2023-10-19 04:32:31 +0530

    Update scrape_cvs.py

--- scrape_cvs.py
+++ scrape_cvs.py
@@ -1,11 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from playwright.sync_api import sync_playwright
 import os
 import time
 import datetime
 import multiprocessing
-# from the selenium librabry import the service module
-from selenium.webdriver.chrome.service import Service
 
 login_url = "https://hiring.naukri.com/hiring/job-listing"
 # if not available then create a config file
@@ -19,104 +16,94 @@ with open("config.txt", "r") as f:
     login_id = lines[0].split("=")[-1].strip()
     passwd = lines[1].split("=")[-1].strip()
 
-
-def login_if_needed(driver:webdriver.Chrome, login_url:str, login_id:str, passwd:str):
-    driver.get(login_url)
-    time.sleep(5)
+def login_if_needed(page, login_url, login_id, passwd):
+    page.goto(login_url)
+    time.sleep(3)
     try:
-        driver.find_element(By.ID, "loginRegTab").click()
+        page.click("id=loginRegTab", timeout=4000)
         time.sleep(3)
-        driver.find_element(By.CLASS_NAME, "username_input.__input").send_keys(login_id)
-        
-        driver.find_element(By.CLASS_NAME, "password_input.__input").send_keys(passwd)
+        # text = "Registered Email ID"
+        page.type('input[name="username"]', login_id)
         # click on submit_button_1  rcom-btn-primary rcom-btn-regular
-        driver.find_element(By.CLASS_NAME, "rcom-btn-primary").click()
+        page.type('input[name="password"]', passwd)
+        time.sleep(2)
+        #button class="submit_button_1  rcom-btn-primary rcom-btn-regular"
+        page.click('button[type="submit"]')
     except Exception as e:
         print("Maybe already logged in")
-    time.sleep(4)
-
-
-def get_session(download_dir=""):
-    # create a new Chrome session ans store it in a directory
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    options.add_argument("--disable-notifications")
-    options.add_argument("--disable-popup-blocking")
-    options.add_argument("--disable-extensions")
-    # set error log to 3
-    if len(download_dir) != 0:  
-        path = os.getcwd()
-        prefs = {"download.default_directory" : os.path.join(path, download_dir)}
-        options.add_experimental_option("prefs",prefs)
-    options.add_argument("--log-level=3")
-    options.add_experimental_option('useAutomationExtension', False)
-    # remove automated control message
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    driver = webdriver.Chrome(options=options, service=Service("chromedriver.exe"))
-    return driver
-
-def get_all_jobs(driver:webdriver.Chrome):
-    driver.refresh()
+    max_wait = 10
+    while True:
+        # get the current url
+        url = page.url
+        if url == login_url:
+            break
+        time.sleep(1)
+        max_wait -= 1
+        if max_wait == 0:
+            break
+def get_all_jobs(page):
+    page.reload()
     time.sleep(4)
     # click on mjrFilterCheckbox
-    driver.find_element(By.CLASS_NAME, "mjrFilterCheckbox").click()
+    page.click('span[class="mjrFilterCheckbox"]')
     time.sleep(1)
     # click on id=mjrSearchBtn
-    driver.find_element(By.ID, "mjrSearchBtn").click()
+    page.click("id=mjrSearchBtn")
     time.sleep(2)
     # get all class="mjrTupleTitle ellipsis"
-    elements = driver.find_elements(By.CLASS_NAME, "mjrTupleTitle.ellipsis")
+    elements = page.query_selector_all('a[class="mjrTupleTitle ellipsis"]')
     links = []
     names = []
     for element in elements:
         links.append(element.get_attribute("href"))
-        names.append(element.text)
+        names.append(element.text_content())
     return links, names
 
-
-def get_all_people(date_to_include:str, driver:webdriver.Chrome, link:str):
-    driver.get(link)
+def get_all_people(date_to_include, page, link):
+    link = f"https://hiring.naukri.com{link}"
+    page.goto(link)
     # click class=label-count
     time.sleep(3)
-    m = driver.find_element(By.CLASS_NAME, "badges-container")
+    m = page.query_selector('div[class="badges-container"]')
     # under m get all divs and click on the first one 
-    m.find_elements(By.TAG_NAME, "div")[0].click()
+    m.query_selector_all('div')[0].click()
     time.sleep(2)
-    driver.find_element(By.CLASS_NAME, "oreFontIcons.ore-arrow_down.ico.ico-expand").click()
+    #span class="sel-text"
+    page.click('span[class="sel-text"]')
     time.sleep(2)
     # find dropdown-options show-count-options
-    e = driver.find_element(By.CLASS_NAME, "dropdown-options.show-count-options")
+    e = page.query_selector('div[class="dropdown-options show-count-options"]')
     # find all li
-    all_li = e.find_elements(By.TAG_NAME, "li")
+    all_li = e.query_selector_all('li')
     # click the second one
     all_li[1].click()
     # scroll to the bottom of the page
     time.sleep(2)
-    num = int(driver.find_element(By.CLASS_NAME, "page-value").text.split(" ")[-1])
+    num = int(page.query_selector('span[class="page-value"]').text_content().split(" ")[-1])
     profiles = []
     dates = []
     while True:
-        if num == 0:
-            break
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        page.evaluate("window.scrollTo(0, document.body.scrollHeight);")
         # now scroll back up slowly to the top
         time.sleep(1)
         for i in range(1, 30):
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight/30*{})".format(i))
+            page.evaluate("window.scrollTo(0, document.body.scrollHeight/30*{})".format(i))
             time.sleep(0.2)
         time.sleep(2)
-        all_people = driver.find_elements(By.CLASS_NAME, "candidate-name")
+        all_people = page.query_selector_all('a[class="candidate-name"]')
         for person in all_people:
             profiles.append(person.get_attribute("href"))
         # get all class="flex-row flex-aic item"
-        all_dates = driver.find_elements(By.CLASS_NAME, "flex-row.flex-aic.item")
+        all_dates = page.query_selector_all('span[class="flex-row flex-aic item"]')
         for d in all_dates:
-            d = d.text.split("Applied on: ")[-1]
+            d = d.text_content().split("Applied on: ")[-1].strip()
             d = datetime.datetime.strptime(d, "%d %b %y").strftime("%d-%m-%y")
             dates.append(d)
-        driver.find_element(By.CLASS_NAME, "oreFontIcons.ore-arrow_down.ico.ico-expand.next").click()
-        time.sleep(2)
         num -= 1
+        if num == 0:
+            break
+        page.click('i[class="oreFontIcons ore-arrow_down ico ico-expand next "]')
+        time.sleep(2)
         # check if last date is less than date_to_include, if it is then break
         if len(date_to_include) != 0:
             if dates[-1] < date_to_include:
@@ -127,38 +114,45 @@ def get_all_people(date_to_include:str, driver:webdriver.Chrome, link:str):
     for d, p in zip(dates, profiles):
         if d >= date_to_include:
             final_dates.append(d)
-            final_profiles.append(p)  
+            final_profiles.append(p)
     return final_profiles
 
-def download_cvs(profiles:list, name:str):
-    # do the login
-    driver = get_session(download_dir=name)
-    login_if_needed(driver, login_url, login_id, passwd)
+def download_cvs(profiles, name,b_name:int):
+    new_session = get_session(f"{b_name}")
+    login_if_needed(new_session, login_url, login_id, passwd)
     for link in profiles:
-        driver.get(link)
+        new_session.goto(link)
         max_tries = 5
         while True:
             time.sleep(1)
             if max_tries == 0:
                 break
             try:
-                driver.find_elements(By.CLASS_NAME, "actionItems")[2].click()
-                # wait till the download is complete
-                time.sleep(3)
+                with new_session.expect_download() as download_info:
+                    new_session.click('i[class="oreFontIcons ore-download icon"]')
+                download = download_info.value
+                download.save_as(f"{name}/{download.suggested_filename}")
                 break
             except:
                 max_tries -= 1
                 continue
-    driver.quit()
+    new_session.close()
+def get_session(name:str):
+    # create a new Chrome session ans store it in a directory
+    p = sync_playwright().start()
+    browser = p.chromium.launch_persistent_context(name, headless=False,args=["--start-maximised"], no_viewport=True)
+    page = browser.new_page()
+    return page
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     to_include = input("Enter date to include in format dd-mm-yy: ")
-    session = get_session()
-    login_if_needed(session, login_url, login_id, passwd)
-    all_jobs,names = get_all_jobs(session)
+    page = get_session("browser_main")
+    login_if_needed(page, login_url, login_id, passwd)
+    all_jobs, names = get_all_jobs(page)
     for job, name in zip(all_jobs, names):
         # create a folder of the job
-        profiles = get_all_people(to_include, session, job)
+        profiles = get_all_people(to_include, page, job)
         try:os.mkdir(name)
         except:pass
         # now split profiles into 4 parts, it can be odd or even so add all extra to the last one
@@ -176,10 +170,10 @@ if __name__ == '__main__':
         # now download all cvs
         threads = []
         for i in profiles:
-            t = multiprocessing.Process(target=download_cvs, args=(i,name,))
+            t = multiprocessing.Process(target=download_cvs, args=(i,name,profiles.index(i),))
             threads.append(t)
             t.start()
         for t in threads:
             t.join()
-    session.quit()
+    page.close()
     print("All done")
